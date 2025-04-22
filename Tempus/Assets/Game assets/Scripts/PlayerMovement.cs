c00ad307919c9b758c2f4dadfba410745d74dcbd
Aaron Ogbemi <aawonny@gmail.com>
2025-04-22 17:43:18 +0100

    Last changes

--- Tempus/Assets/Game assets/Scripts/PlayerMovement.cs
+++ Tempus/Assets/Game assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     private Rigidbody2D _playerRigidbody;
     Vector2 movement;
+    Vector2 direction;
+    float targetAngle;
 
     
     void Start()
@@ -16,7 +18,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // This is the best I could come up with for now Doesn't work at all sha
+        /*
+        direction = _playerRigidbody.velocity.normalized;
+        targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, targetAngle - 90));
+        */
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
